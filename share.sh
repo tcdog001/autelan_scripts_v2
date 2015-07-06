e28b8ab6237d7ee52690375b27011551ca889103
liuhj <liuhuijun@debian6.(none)>
2015-07-06 17:04:52 -0400

    add share.sh ap in ap_make()

--- share.sh
+++ share.sh
@@ -16,21 +16,21 @@ md_install() {
 
 ap_install() {
 	local src=../custom
-	local dst=../trunk_project/trunk/package/autelan-shell/src/jsock/etc/
+	local dst=../ap_project/qsdk/package/autelan-shell/src/jsock/etc
  
-	rm -fr ${dst}/jsock/*
-	rm -fr ${dst}/utils/*
-	rm -fr ${dst}/xinetd.d/*
-	rm -fr ${dst}/stimer/*
-	rm -fr ${dst}/fagent/*
+	rm -fr ${dst}/jsock
+	rm -fr ${dst}/utils
+	rm -fr ${dst}/xinetd.d
+	rm -fr ${dst}/stimer
+	rm -fr ${dst}/fagent
 
 	echo "copy files from ${src}/etc to ${dst}"
-	cp -fpR ${src}/etc/jsock/* ${dst}/jsock
-	cp -fpR ${src}/etc/utils/* ${dst}/utils
-	cp -fpR ${src}/etc/xinetd.d/* ${dst}/xinetd.d
-	cp -fpR ${src}/etc/stimer/* ${dst}/stimer
-	cp -fpR ${src}/etc/fagent/* ${dst}/fagent
-	cp -fpR ${src}/usr/sbin/sysalarm ${dst}/../usr/sbin
+	cp -fpR ${src}/etc/jsock ${dst}
+	cp -fpR ${src}/etc/utils ${dst}
+	cp -fpR ${src}/etc/xinetd.d ${dst}
+	#cp -fpR ${src}/etc/stimer ${dst}
+	#cp -fpR ${src}/etc/fagent ${dst}
+	#cp -fpR ${src}/usr/sbin/sysalarm ${dst}/../usr/sbin
 }
 
 main() {
