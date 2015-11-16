8fdd2b7640fa5c12fe8022d33ee08b6cdbb02605
ljf10000 <l_j_f@163.com>
2015-11-16 17:54:22 -0500

    .

--- pullall.sh
+++ pullall.sh
@@ -4,14 +4,29 @@ if [ -z "${hisitopdir}" ]; then
 	export hisitopdir=$(hisitopdir.sh $0)
 fi 
 
-main() {
-	local dir
 
-	while read dir; do
-		pushd ${hisitopdir}/${dir} &> /dev/null
+pull() {
+	local dir="$1"
+
+	if [[ -d ${dir} ]]; then
+		pushd ${dir} &> /dev/null
 		echo "pull ${dir}"
 		git pull
 		popd &> /dev/null
+	fi
+}
+
+main() {
+	local dir
+	local list
+
+	list="${hisitopdir}/../project_tools ${hisitopdir}/../project/project_tools"
+	for dir in ${list}; do
+		pull ${dir}
+	done
+
+	while read dir; do
+		pull ${hisitopdir}/${dir}
 	done < ${hisitopdir}/list.in
 }
 
